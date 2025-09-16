e9cf216d3ef171f5c3aaa17a0e11db72d883a488
arthurtuts152 <arthurromanoluz@gmail.com>
2025-09-15 22:13:50 -0300

    feat(frontend): wip inicio da implementação do about

--- Frontend/SYSOCIAL/src/app/features/about/about.component.ts
+++ Frontend/SYSOCIAL/src/app/features/about/about.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ZardDividerComponent } from '@shared/components/divider/divider.component';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [ZardDividerComponent], 
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  
 }
