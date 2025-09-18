43f8a23b83c66b1b780e7bf8a885808e5863cc98
Apolinari0 <lucasapolinario070@gmail.com>
2025-09-17 21:22:37 -0300

    feat(frontend): Footer para a aplicação

--- Frontend/SYSOCIAL/src/app/core/layout/layout.component.ts
+++ Frontend/SYSOCIAL/src/app/core/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
