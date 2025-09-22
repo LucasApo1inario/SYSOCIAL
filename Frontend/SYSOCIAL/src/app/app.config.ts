cf435a27fb4226cd92c616e2c23ef8ffcb01508f
Apolinari0 <lucasapolinario070@gmail.com>
2025-09-21 21:23:19 -0300

    feat(frontend): separando funcionalidades em devidos lugares

--- Frontend/SYSOCIAL/src/app/app.config.ts
+++ Frontend/SYSOCIAL/src/app/app.config.ts
@@ -1,17 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideLoggedInUser } from './core/auth/initializers/provide-logged-in-user';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { setAuthTokenInterceptor } from './core/auth/interceptors/set-auth-token-interceptor';
+import { provideCore } from './core/provide-core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideLoggedInUser(),
-    provideHttpClient(withInterceptors([setAuthTokenInterceptor]))
+    provideCore()
   ]
 };
