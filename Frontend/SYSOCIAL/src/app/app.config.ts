3b95b2efa9b3cdc33418e9fcbe35fa8e65c1ee9f
Apolinari0 <lucasapolinario070@gmail.com>
2025-09-21 21:09:26 -0300

    feat(frontend): interceptors para adicao de token

--- Frontend/SYSOCIAL/src/app/app.config.ts
+++ Frontend/SYSOCIAL/src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideLoggedInUser } from './core/auth/initializers/provide-logged-in-user';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { setAuthTokenInterceptor } from './core/auth/interceptors/set-auth-token-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideLoggedInUser()
+    provideLoggedInUser(),
+    provideHttpClient(withInterceptors([setAuthTokenInterceptor]))
   ]
 };
