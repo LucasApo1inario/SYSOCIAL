9e99059c304ab5d04cb84c6adba5314f7f434823
arthurtuts152 <arthurromanoluz@gmail.com>
2025-10-06 23:29:47 -0300

    feat: inicio do about

--- Frontend/SYSOCIAL/src/app/features/about/about.component.ts
+++ Frontend/SYSOCIAL/src/app/features/about/about.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
  
 import { ZardAccordionComponent } from '@shared/components/accordion/accordion.component';
 import { ZardAccordionItemComponent } from '@shared/components/accordion/accordion-item.component';
- 
+
 @Component({
   standalone: true,
   imports: [ZardAccordionComponent, ZardAccordionItemComponent],
