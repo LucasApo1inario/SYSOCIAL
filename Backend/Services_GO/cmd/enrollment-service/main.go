b4cb33683a44b87cd055f7b7728de68e1f4306d8
Bruno de Abreu Correia <b.abreuc@outlook.com>
2025-11-28 21:52:32 -0300

    fix: remoção do CORS no enrollment service

--- Backend/Services_GO/cmd/enrollment-service/main.go
+++ Backend/Services_GO/cmd/enrollment-service/main.go
@@ -49,20 +49,6 @@ func main() {
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	// CORS
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH") // ADICIONADO PATCH
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})
-
 	// Rotas
 	v1 := router.Group("/api/v1")
 	{
