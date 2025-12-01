acb6e887caf9c4af312659a1b423accdf29dd7c5
Arthur Losano <arthur@assystem.com.br>
2025-12-01 19:37:41 -0300

    ultimos commits

--- Backend/Services_GO/internal/shared/middleware/middleware.go
+++ Backend/Services_GO/internal/shared/middleware/middleware.go
@@ -64,7 +64,7 @@ func CORS() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
